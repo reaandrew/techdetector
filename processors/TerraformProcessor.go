fb34c840bfe5280b0c43a00b011b829b2673bb77
Andy Rea <email@andrewrea.co.uk>
2025-01-07 14:05:35 +0000

    Added Azure, GCP and AWS resource block processors

--- processors/TerraformProcessor.go
+++ processors/TerraformProcessor.go
@@ -51,7 +51,7 @@ func (a AWSResourceBlockProcessor) Process(block *TerraformBlock, path string, r
 	if block.Type == "resource" && len(block.Labels) > 0 && strings.HasPrefix(block.Labels[0], "aws_") {
 		matches = append(matches, core.Finding{
 			Name:     "AWS Resource",
-			Type:     "AWS Resource Use",
+			Type:     "Terraform Resource Use",
 			Category: "AWS", // or whatever category you prefer
 			Properties: map[string]interface{}{
 				"resource_type": block.Labels[0],
@@ -75,7 +75,7 @@ func (a AzureResourceBlockProcessor) Process(block *TerraformBlock, path string,
 	if block.Type == "resource" && len(block.Labels) > 0 && strings.HasPrefix(block.Labels[0], "azurerm_") {
 		matches = append(matches, core.Finding{
 			Name:     "Azure Resource",
-			Type:     "Azure Resource Use",
+			Type:     "Terraform Resource Use",
 			Category: "Azure",
 			Properties: map[string]interface{}{
 				"resource_type": block.Labels[0],
@@ -99,7 +99,7 @@ func (g GCPResourceBlockProcessor) Process(block *TerraformBlock, path string, r
 	if block.Type == "resource" && len(block.Labels) > 0 && strings.HasPrefix(block.Labels[0], "google_") {
 		matches = append(matches, core.Finding{
 			Name:     "GCP Resource",
-			Type:     "GCP Resource Use",
+			Type:     "Terraform Resource Use",
 			Category: "GCP",
 			Properties: map[string]interface{}{
 				"resource_type": block.Labels[0],
