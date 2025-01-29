bc1b71b05a32a62f0ccb1f0c923a9d4f22524d1e
Andy Rea <email@andrewrea.co.uk>
2025-01-29 09:34:38 +0000

    Fix the tests

--- utils/Db.go
+++ utils/Db.go
@@ -5,38 +5,7 @@ import (
 	"encoding/json"
 	"fmt"
 	"log"
-	"os"
 )
-
-func DumpSQLiteSchema(db_file string) {
-	db, err := sql.Open("sqlite3", db_file)
-	if err != nil {
-		log.Fatalf("Failed to open database: %v", err)
-	}
-	defer db.Close()
-
-	rows, err := db.Query("SELECT sql FROM sqlite_master WHERE type='table'")
-	if err != nil {
-		log.Fatalf("Failed to query schema: %v", err)
-	}
-	defer rows.Close()
-
-	file, err := os.Create("schema.txt")
-	if err != nil {
-		log.Fatalf("Failed to create schema file: %v", err)
-	}
-	defer file.Close()
-
-	for rows.Next() {
-		var sql string
-		if err := rows.Scan(&sql); err != nil {
-			log.Fatalf("Failed to read schema row: %v", err)
-		}
-		file.WriteString(sql + "\n")
-	}
-
-	log.Println("Schema dumped to schema.txt")
-}
 
 func DumpSQLiteSchemaForFindings(db_file string) {
 	db, err := sql.Open("sqlite3", db_file)
@@ -47,8 +16,9 @@ func DumpSQLiteSchemaForFindings(db_file string) {
 
 	query := "SELECT Properties FROM findings;"
 	rows, err := db.Query(query)
+
 	if err != nil {
-		log.Fatalf("failed to query properties: %w", err)
+		log.Fatalf("failed to query properties: %v", err)
 	}
 	defer rows.Close()
 
