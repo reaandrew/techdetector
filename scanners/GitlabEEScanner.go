185c92a8cc15144e7f0ad8af5b0a978f74c16897
Andy Rea <email@andrewrea.co.uk>
2025-03-06 22:59:00 +0000

    Try admin permissions

--- scanners/GitlabEEScanner.go
+++ scanners/GitlabEEScanner.go
@@ -175,7 +175,7 @@ func listAllProjects(client *gitlab.Client) ([]*gitlab.Project, error) {
 	var allProjects []*gitlab.Project
 	ctx := context.Background()
 	opts := &gitlab.ListProjectsOptions{
-		MinAccessLevel: gitlab.Ptr(gitlab.GuestPermissions),
+		MinAccessLevel: gitlab.Ptr(gitlab.AdminPermissions),
 		ListOptions: gitlab.ListOptions{
 			Page:    1,
 			PerPage: 100,
