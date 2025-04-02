fc800e3e1d5466300fbb10e805cdde10128ba670
Andy Rea <email@andrewrea.co.uk>
2025-04-02 10:03:42 +0100

    Remove non library components

--- scripts/invoke_lambda.sh
+++
@@ -1,8 +0,0 @@
-aws lambda invoke \
-  --function-name lambda_techdetector \
-  --region eu-west-2 \
-  --cli-binary-format raw-in-base64-out \
-  --payload '{
-    "body": "{\"repo\": \"https://github.com/reaandrew/techdetector.git\"}"
-  }' \
-  output.json
